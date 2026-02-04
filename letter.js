9495725c949399cfff5de61140a9ccfadbfd0eaa
Siam-moni <shariars012@gmail.com>
2026-02-04 22:07:31 +0600

    letter a A task

--- letter.js
+++ letter.js
@@ -1,11 +1,20 @@
-let letter = "Hi My name is Siam";
-let count = 0;
-// for (let i = 0; i < letter.length; i++) {
-//   if (letter[i] === "a") count++;
+// let letter = "Hi My name is Siam";
+// let count = 0;
+// // for (let i = 0; i < letter.length; i++) {
+// //   if (letter[i] === "a") count++;
+// // }
+// // console.log(count);
+// for (let i of letter) {
+//   if (i === "a") {
+//     count++;
+//   }
 // }
 // console.log(count);
-for (let i of letter) {
-  if (i === "a") {
+
+let letters = "Hi My name is SiAm eAting banAna";
+let count = 0;
+for (let i of letters) {
+  if (i === "a" || i === "A") {
     count++;
   }
 }
